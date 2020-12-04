691f44e07d89437db8c4b2181965cc963ac849a1
pycrash <=>
2020-12-05 01:39:35 +0530

    Search Shipper

--- app/src/main/java/com/ashu/eatitserver/ui/shipper/ShipperFragment.java
+++ app/src/main/java/com/ashu/eatitserver/ui/shipper/ShipperFragment.java
@@ -1,10 +1,13 @@
 package com.ashu.eatitserver.ui.shipper;
 
+import androidx.appcompat.widget.SearchView;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -15,10 +18,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LayoutAnimationController;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.ashu.eatitserver.Adapter.MyOrderAdapter;
@@ -26,6 +34,7 @@ import com.ashu.eatitserver.Adapter.MyShipperAdapter;
 import com.ashu.eatitserver.Common.Common;
 import com.ashu.eatitserver.EventBus.ChangeMenuClick;
 import com.ashu.eatitserver.EventBus.UpdateShipperEvent;
+import com.ashu.eatitserver.Model.FoodModel;
 import com.ashu.eatitserver.Model.ShipperModel;
 import com.ashu.eatitserver.R;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -36,6 +45,7 @@ import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -58,6 +68,7 @@ public class ShipperFragment extends Fragment {
     LayoutAnimationController layoutAnimationController;
     MyShipperAdapter adapter;
     List<ShipperModel> shipperModelList;
+    private List<ShipperModel> saveShipperBeforeSearchList;
 
     public static ShipperFragment newInstance() {
         return new ShipperFragment();
@@ -77,6 +88,8 @@ public class ShipperFragment extends Fragment {
         shipperViewModel.getShipperMutableList().observe(getViewLifecycleOwner(), shippers -> {
             dialog.dismiss();
             shipperModelList = shippers;
+            if (saveShipperBeforeSearchList == null)
+                saveShipperBeforeSearchList = shippers;
             adapter = new MyShipperAdapter(getContext(), shipperModelList);
             recycler_shipper.setAdapter(adapter);
             recycler_shipper.setLayoutAnimation(layoutAnimationController);
@@ -84,7 +97,58 @@ public class ShipperFragment extends Fragment {
         return itemView;
     }
 
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.food_list_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+
+        SearchManager searchManager = (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getActivity().getComponentName()));
+
+
+        //event
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                startSearchShipper(s);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+
+        //clear text when clear text is pressed
+        ImageView closeButton = searchView.findViewById(R.id.search_close_btn);
+        closeButton.setOnClickListener(view -> {
+            EditText ed = searchView.findViewById(R.id.search_src_text);
+            //Clear Text
+            ed.setText("");
+            searchView.setQuery("", false);
+            searchView.onActionViewCollapsed();
+            menuItem.collapseActionView();
+            if (saveShipperBeforeSearchList != null)
+                shipperViewModel.getShipperMutableList().setValue(saveShipperBeforeSearchList);
+        });
+    }
+
+    private void startSearchShipper(String s) {
+        List<ShipperModel> resultShipper = new ArrayList<>();
+        for (int i = 0; i < shipperModelList.size(); i++) {
+
+            ShipperModel shipperModel = shipperModelList.get(i);
+            if (shipperModel.getPhone().toLowerCase().contains(s.toLowerCase())) {
+                resultShipper.add(shipperModel);
+            }
+        }
+        shipperViewModel.getShipperMutableList().setValue(resultShipper);
+    }
+
     private void initViews() {
+        setHasOptionsMenu(true);
         dialog = new SpotsDialog.Builder().setCancelable(false).setContext(getContext()).build();
         layoutAnimationController = AnimationUtils.loadLayoutAnimation(getContext(), R.anim.layout_item_from_left);
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
